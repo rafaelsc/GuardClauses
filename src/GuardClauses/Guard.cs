da651a6de6d10735cc698917c46388db7b5cb82c
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2021-10-31 16:54:46 -0700

    Add IGuardDebugClause

--- src/GuardClauses/Guard.cs
+++ src/GuardClauses/Guard.cs
@@ -9,16 +9,22 @@ namespace Ardalis.GuardClauses
     {
     }
 
+    public interface IGuardDebugClause
+    {
+    }
+
     /// <summary>
     /// An entry point to a set of Guard Clauses defined as extension methods on IGuardClause.
     /// </summary>
     /// <remarks>See http://www.weeklydevtips.com/004 on Guard Clauses</remarks>
-    public class Guard : IGuardClause
+    public class Guard : IGuardClause, IGuardDebugClause
     {
         /// <summary>
         /// An entry point to a set of Guard Clauses.
         /// </summary>
         [JetBrainsNotNull] public static IGuardClause Against { get; } = new Guard();
+
+        [JetBrainsNotNull] public static IGuardDebugClause DebugAgainst { get; } = new Guard();
 
         private Guard() { }
     }
---
+++ src/GuardClauses/GuardDebug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using JetBrainsNoEnumerationAttribute = JetBrains.Annotations.NoEnumerationAttribute;
+using JetBrainsNotNullAttribute = JetBrains.Annotations.NotNullAttribute;
+
+namespace Ardalis.GuardClauses
+{
+    public static class GuardDebug
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not null.</returns>
+        [Conditional("DEBUG")]
+        public static void Null<T>([JetBrainsNotNull] this IGuardDebugClause guardClause, [NotNull, JetBrainsNotNull][ValidatedNotNull][JetBrainsNoEnumeration] T input, [JetBrainsNotNull] string parameterName, string? message = null)
+        {
+            Guard.Against.Null(input, parameterName, message);
+        }
+    }
+}
